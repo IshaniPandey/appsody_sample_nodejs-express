dd06c95350403b71c7e5c052d918b10ebf3b0415
ispandey <ispandey@in.ibm.com>
2020-04-15 20:19:19 +0530

    test2

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const app = require('express')()
 var dash = require('appmetrics-dash').monitor();
 app.get('/', (req, res) => {
-  res.send("Hello from Cloud Pak!");
+  res.send("Hello from Cloud Pak for Applications!");
 });
  
 module.exports.app = app;
